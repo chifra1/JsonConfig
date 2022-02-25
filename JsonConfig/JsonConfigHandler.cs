b0acc0daaaae5f33f7107e8fc623184ce85dc2ae
chifra <chifra@ERP2MOB>
2022-02-25 10:33:33 +0100

    Add project files.

---
+++ JsonConfig/JsonConfigHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Utils
+{
+    public class JsonConfigHandler<T> where T : JsonConfig
+    {
+
+        private string _filename = "config.json";
+
+        public JsonConfigHandler()
+        {
+        }
+        public JsonConfigHandler(string filename) : base()
+        {
+            _filename = string.IsNullOrWhiteSpace(filename) ? _filename : filename.Trim();
+        }
+        public void WriteExample(T example)
+        {
+            File.WriteAllText(_filename+".example", JsonConvert.SerializeObject(example, new JsonSerializerSettings { Formatting = Newtonsoft.Json.Formatting.Indented }));
+        }
+        public void Write(T config)
+        {
+            File.WriteAllText(_filename, JsonConvert.SerializeObject(config, new JsonSerializerSettings { Formatting = Newtonsoft.Json.Formatting.Indented }));
+        }
+        public JsonConfig? Load()
+        {
+            JsonConfig? _ret = null;
+            //load configuration file
+            try
+            {
+                _ret = JsonConvert.DeserializeObject<JsonConfig>(File.ReadAllText(_filename));                
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(_filename + ".log", "Wrong config file\n\n" + ex.Message);
+            }
+            return _ret; 
+        }
+    }
+    public class JsonConfig
+    { 
+    
+    }
+}
---
+++ JsonConfigExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Utils;
+
+namespace UtilsExamples 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            JsonConfigHandler<JsonConfigExample> handler = new JsonConfigHandler<JsonConfigExample>("settings.cfg");
+            
+            //to get an example of json config file
+            JsonConfigExample example = new JsonConfigExample() { Mailfrom = "ddd@example.org", Mailto = "aaa@example.org,bbb@example.org", ServerSmtp = "mail.example.org", Port = 555 };
+            handler.WriteExample(example);
+            
+            //load a json configuration file
+            handler.Load();
+        }
+    }
+    /// <summary>
+    /// Example of Poco class to manage configuration settings
+    /// </summary>
+    public class JsonConfigExample : JsonConfig
+    {
+        public string? Mailfrom { get; set; }
+        public string? Mailto { get; set; }
+        public string? ServerSmtp { get; set; }
+        public int Port { get; set; }
+    }
+}
